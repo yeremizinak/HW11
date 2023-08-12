f68df38432efc77d14181bf44e076e625fdcf667
yeremizinak <eremizinaekaterina@gmail.com>
2023-08-12 15:49:37 +0300

    fix

--- HW11/src/main/java/org/example/Task1.java
+++ HW11/src/main/java/org/example/Task1.java
@@ -8,8 +8,8 @@ import java.util.stream.IntStream;
 public class Task1 {
     public static String odd(List<String> names) {
         return IntStream.range(0, names.size())
-                .filter(i -> i % 2 == 0)
-                .mapToObj(i -> (i + 1) + ". " + names.get(i))
+                .filter(i -> i % 2!= 0)
+                .mapToObj(i -> i + ". " + names.get(i))
                 .collect(Collectors.joining(", "));
     }
 }
--- HW11/src/main/java/org/example/Task3.java
+++ HW11/src/main/java/org/example/Task3.java
@@ -1,23 +1,21 @@
 package org.example;
 
+import java.rmi.server.ObjID;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Task3 {
     public static String task3(String[] array) {
-        List<String> numbers = new ArrayList<>();
-        for (int i = 0; i < array.length; i++) {
-            String[] tempArr = array[i].split(",\\s");
-            for (int j = 0; j < tempArr.length; j++) {
-                numbers.add(tempArr[j]);
-            }
-        }
-
-        return numbers
-                .stream()
+        String result = Arrays.stream(array)
+                .flatMap(s -> Arrays.stream(s.split(",\\s")))
+                .map(Integer::parseInt)
                 .sorted()
+                .map(Object::toString)
                 .collect(Collectors.joining(", "));
 
+        return result;
     }
 }
